2e277aeb2a9d0b1c3d72af216d3c67192a7294e0
ADSFAaron <aaron881015@hotmail.com.tw>
2021-06-04 15:27:56 +0800

    Home Page Import to Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', \App\Http\Livewire\GamePlatform::class);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
