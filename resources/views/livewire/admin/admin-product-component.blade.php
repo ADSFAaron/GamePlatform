97e75febb9a86796fcc7f5ef07b29793e159edae
s1073327 <k04657128930@gmail.com>
2021-06-10 14:30:28 +0800

    git:https://github.com/s1073327

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -1,5 +1,5 @@
+{{-- Admin Product頁面css --}}
 <div>
-    //Admin Product頁面css
     <style>
         nav svg{
             height: 20px;
@@ -11,37 +11,53 @@
     <div class="container" style="padding:30px 0;">
         <div class="row">
             <div class="col-md-12">
-                <div class="panel-heading">
-                    All Products
-                </div>
-                <div class="panel-body">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <td>Id</td>
-                                <td>Image</td>
-                                <td>Name</td>
-                                <td>Stock</td>
-                                <td>Price</td>
-                                <td>Category</td>
-                                <td>Date</td>
-                                <td>Action</td>
-                            </tr>
-                        </thead>
-                        @foreach ($products as $product)
-                            <tr>
-                                <td>{{$product->id}}</td>
-                                <td><img src="{{asset('assets/images/products')}}/{{$product->image}}" width="60"></td>
-                                <td>{{$product->name}}</td>
-                                <td>{{$product->stock_status}}</td>
-                                <td>{{$product->regular_price}}</td>
-                                <td>{{$product->category->name}}</td>
-                                <td>{{$product->created_at}}</td>
-                                <td></td>
-                            </tr>
-                        @endforeach
-                    </table>
-                    {{$products->links()}}
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                All Products
+                            </div>
+                            <div>
+                                <a href="{{route('admin.addproduct')}}" class="btn btn-success pull-right">Add New</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel-body">
+                        @if (Session::has('message'))
+                            <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                        @endif
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Stock</th>
+                                    <th>Price</th>
+                                    <th>Category</th>
+                                    <th>Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            @foreach ($products as $product)
+                                <tr>
+                                    <td>{{$product->id}}</td>
+                                    <td><img src="{{asset('assets/images/products')}}/{{$product->image}}" width="60"></td>
+                                    <td>{{$product->name}}</td>
+                                    <td>{{$product->stock_status}}</td>
+                                    <td>{{$product->regular_price}}</td>
+                                    <td>{{$product->category->name}}</td>
+                                    <td>{{$product->created_at}}</td>
+                                    <td>
+                                        <a href="{{route('admin.editproduct',['product_slug'=>$product->slug])}}"><i class="fa fa-edit fa-2x text-info"></i></a>
+                                        <a href="#" style="margin-left:10px;" wire:click.prevent="deleteProduct({{$product->id}})"><i class="fa fa-times fa-2x text-danger"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                        {{$products->links()}}
+                    </div>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 use Illuminate\Support\Facades\Route;
 
 use \App\Http\Livewire\GamePlatform;
@@ -15,6 +16,7 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 
 
@@ -65,5 +67,6 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
     Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory') ;
     Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
     Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
-
+    Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
+    Route::get('/admin/product/edit/{product_slug}',AdminEditProductComponent::class)->name('admin.editproduct');
 });
